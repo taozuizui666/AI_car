5d7f86d08b39a2a2e92e4955fcb312f0462ac1cf
陶然 <3612759815@qq.com>
2025-09-09 10:13:22 +0800

    Lidar APP which wrote myself can scan the arround environment like the official APP does

--- test_serial.py
+++ test_serial.py
@@ -1,9 +1,14 @@
 import serial
+import math
+import numpy as np
 from matplotlib import pyplot as plt
 
 Lidar_data_bin_num = 5
 Lidar_head_num = 7
 ser = None
+data_buff_num = 1000
+angle_buff = []
+dist_buff = []
 
 while not ser:
     try:
@@ -13,31 +18,33 @@ while not ser:
 
 data = ser.read(Lidar_head_num)
 head_values = ' '.join(f'{b:02X}' for b in data)
+
 if head_values =="A5 5A 05 00 00 40 81":
     print("yes")
-
-    # plt.ion()  # 打开交互模式
-    # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    # ax.set_theta_zero_location('N')  # 设置0度方向为北（向上）
-    # ax.set_ylim(0, 10)  # 距离范围，根据实际情况调整
-    # line, = ax.plot([], [], 'ro')  # 初始化线对象
-    # background = fig.canvas.copy_from_bbox(ax.bbox)  # 保存背景
+    # plt.ion()
+    # fig,ax = plt.subplots(subplot_kw={'projection':'polar'})
+    # ax.set_theta_zero_location('N')  # 设置 0 度在上方 (北)
+    # ax.set_theta_direction(-1)      # 设置角度顺时针增加
+    # ax.set_ylim(0, 10)               # 设置距离显示范围 (米)，根据需要调整
+    # ax.set_title("RPLidar 实时扫描图")
+    # scat = ax.scatter([], [], c='b', s=5) # s 是点的大小
 
     while True:
         data = ser.read(Lidar_data_bin_num)
-        if len(data) == Lidar_data_bin_num:  
+        if len(data) == Lidar_data_bin_num:
             first_byte = data[0]
             angle_raw = ((data[2]<<8) | data[1]) >>1
             distance_raw = (data[4]<<8) | data[3]
             angle_real = angle_raw/64.
             distance_real = distance_raw/4000.
             print(f"Angle: {angle_real:.2f} deg, Distance: {distance_real:.3f} m\n")
-
-            # # 更新数据
-            # line.set_xdata([angle_real * 3.1415926 / 180])  # 将角度转换为弧度
-            # line.set_ydata([distance_real])
-
-            # # 使用 blit 技术加速绘图
-            # fig.canvas.restore_region(background)  # 恢复背景
-            # ax.draw_artist(line)  # 重绘线条
-            # fig.canvas.blit(ax.bbox)  # 更新变动部分
+            
+            # angle_rad = math.radians(angle_real)
+            # angle_buff.append(angle_rad)
+            # dist_buff.append(distance_real)
+            # if len(angle_buff)>data_buff_num:
+            #     angle_buff.pop(0)
+            #     dist_buff.pop(0)
+            # scat.set_offsets(np.c_[angle_buff, dist_buff])
+            # fig.canvas.draw()
+            # fig.canvas.flush_events()
